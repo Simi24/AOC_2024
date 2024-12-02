75691f2acbfd5a89567d37b066b0995953a24e9f
Simi24 <pettasimonepaolo@gmail.com>
2024-12-02 12:59:19 +0100

    day 2 solution

---
+++ day2/solution.py
@@ -0,0 +1,60 @@
+reports = []
+
+def parseInput():
+    with open("input.txt") as file:
+        for line in file:
+            array = line.split()
+            temp = []
+            for i in range(len(array)):
+                temp.append(int(array[i]))
+            
+            reports.append(temp)
+
+#PART 1
+def isIncreasing(array):
+    for i in range(len(array)):
+        if i == 0:
+            continue
+        if array[i] <= array[i-1]:
+            return False
+    return True
+
+def isDecreasing(array):
+    for i in range(len(array)):
+        if i == 0:
+            continue
+        if array[i] >= array[i-1]:
+            return False
+    return True
+
+def isValidReport(report):
+    if isDecreasing(report) or isIncreasing(report):
+        for i in range(len(report)):
+            if i == 0:
+                continue
+            if abs(report[i] - report[i-1]) > 3:
+                return False
+        return True
+
+#PART 2
+def isValidReportRemovingElement(report):
+    temp = report
+    for i in range(len(report)):
+        temp = report[:i] + report[i+1:]
+        if isValidReport(temp):
+            return True
+    return False
+
+def main():
+    parseInput()
+    validReports = 0
+    for report in reports:
+        if isValidReport(report):
+            validReports += 1
+        else:
+            if isValidReportRemovingElement(report):
+                validReports += 1
+    print("Valid reports: ", validReports)
+
+if __name__ == "__main__":
+    main()
